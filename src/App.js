0538612d5ddd6ce64a9f566579ed6cca4ea51e4e
marismcg <mcguinness.m@northeastern.edu>
2022-09-16 09:37:36 -0400

    a2 lab completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+function WelcomeToFSD() {
+  return null;
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <WelcomeToFSD/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
