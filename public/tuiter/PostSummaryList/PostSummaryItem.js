69f205ed7cce29895c3ec6f1c6f4dc02be696369
marismcg <mcguinness.m@northeastern.edu>
2022-10-12 18:10:08 -0400

    fix post summary list item

--- public/tuiter/PostSummaryList/PostSummaryItem.js
+++ public/tuiter/PostSummaryList/PostSummaryItem.js
@@ -1,19 +1,21 @@
 const PostSummaryItem = (post) => {
     return(`
     <div class="card">
-        <div class="row">
+        <div class="row align-items-center">
             <div class="col-9">
-                <p>${post.topic}</p>
-                <span>
-                    <b>${post.username}</b>
+                <div class="card-body">
+                <p class="mb-0 text-secondary">${post.topic}</p>
+                <span class="d-flex align-items-center">
+                    <b class="text-white">${post.userName}</b>
                     <i class="m-1 fa-solid fa-circle-check fa-sm"></i>
-                    <p>${post.time}</p>
+                    <p class="mb-0 text-secondary">- ${post.time}</p>
                 </span>
-                <b>${post.title}</b>
-                <p>${post.tweets}</p>
+                <b class="text-white">${post.title}</b>
+                <p class="mb-0 text-secondary">${post.tweets}</p>
+                </div>
             </div>
             <div class="col-3">
-                <img src="${post.image}">
+                <div class="p-3"><img class="img-fluid rounded" src="${post.image}"></div>
             </div>
         </div>
     </div>
---
+++ public/tuiter/explore/ExploreComponent.js
@@ -0,0 +1,15 @@
+import index from "../PostSummaryList/index.js";
+
+const ExploreComponent = () => {
+    return(`
+            <div class="row">
+                       <!-- search field and cog -->
+           </div>
+           <ul class="nav mb-2 nav-tabs">
+                      <!-- tabs -->
+           </ul>
+           <!-- image with overlaid text -->
+           ${index()}
+    `);
+}
+export default ExploreComponent;
