a1db6d0cb00d390a76923074f17fad95134a8ac4
marismcg <mcguinness.m@northeastern.edu>
2022-11-03 14:54:52 -0400

    impl A7 lab

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import Labs from "./labs";
-import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter";
 import {BrowserRouter} from "react-router-dom";
 import {Routes, Route} from "react-router";
+import Assignment7 from "./labs/a7";
 
 function App() {
     return (
@@ -11,8 +11,7 @@ function App() {
                 <Routes>
                     <Route index
                            element={<Labs/>}/>
-                    <Route path="/hello"
-                           element={<HelloWorld/>}/>
+                    <Route path="/a7" element={<Assignment7/>}/>
                     <Route path="/tuiter"
                            element={<Tuiter/>}/>
                 </Routes>
